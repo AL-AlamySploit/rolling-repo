84865840e350312cb3d425e1e3d291724975f478
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-08-29 03:29:40 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+A='\033[01;32m'
+clear
+pkg install python
+pkg install git
+cd
+rm -rif termux-packages
+git clone https://github.com/AL-AlamySploit/rolling-repo
+clear
+echo "Type: cd "
+echo "$A[+] Done..!"
+exit
